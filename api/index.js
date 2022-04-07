c5a522241ac4d3162708a886ce8767db075a40ea
Soheil Qorbani <soheil.q.k@gmail.com>
2022-04-07 19:59:14 +0430

    auth route added

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
+const authRoute = require("./routes/auth");
+
 dotenv.config();
 const app = express();
 mongoose
@@ -11,6 +13,10 @@ mongoose
   })
   .then(() => console.log("DB connected successfully"))
   .catch((err) => console.log("DB error", err));
+
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
 
 app.listen(8800, () => {
   console.log("backend server is running");
