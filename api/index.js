aa3935b4cfc1d84131b8af9cd1a6bda0c6956002
Soheil Qorbani <soheil.q.k@gmail.com>
2022-04-08 00:47:51 +0430

    movies and list routes

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv");
 
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
+const movieRoute = require("./routes/movies");
 
 dotenv.config();
 const app = express();
@@ -19,6 +20,7 @@ app.use(express.json());
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/movies", movieRoute);
 
 app.listen(8800, () => {
   console.log("backend server is running");
