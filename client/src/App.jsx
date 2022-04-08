9b5c328072966b463c1178ad16a9210fab291f96
Soheil Qorbani <soheil.q.k@gmail.com>
2022-04-08 18:08:04 +0430

    routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,9 +3,46 @@ import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import Watch from "./pages/watch/Watch";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 const App = () => {
-  return <Login />;
+  function Redirect({ to }) {
+    let navigate = useNavigate();
+    useEffect(() => {
+      navigate(to);
+    });
+    return null;
+  }
+
+  const user = true;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          exact
+          path="/"
+          element={user ? <Home /> : <Redirect to="/register" />}
+        />
+        <Route
+          path="/register"
+          element={!user ? <Register /> : <Redirect to="/" />}
+        />
+        <Route
+          path="/login"
+          element={!user ? <Login /> : <Redirect to="/" />}
+        />
+        {user && (
+          <>
+            <Route path="/movies" element={<Home type="movies" />} />
+            <Route path="/series" element={<Home type="series" />} />
+            <Route path="/watch" element={<Watch />} />
+          </>
+        )}
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
