831db2bf4f487bf642b9cca174dea37b51365217
Soheil Qorbani <soheil.q.k@gmail.com>
2022-05-24 19:03:45 +0430

    users page

--- admin/src/App.js
+++ admin/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Topbar from "./components/Topbar/Topbar";
 import Home from "./pages/Home/Home";
+import UserList from "./pages/UserList/UserList";
 
 function App() {
   return (
@@ -11,7 +12,10 @@ function App() {
         <Topbar />
         <div className="container">
           <Sidebar />
-          <Home />
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/users" element={<UserList />} />
+          </Routes>
         </div>
       </div>
     </BrowserRouter>
--- admin/src/components/Sidebar/Sidebar.jsx
+++ admin/src/components/Sidebar/Sidebar.jsx
@@ -13,7 +13,7 @@ import {
   WorkOutline,
   Report,
 } from "@mui/icons-material";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Sidebar = () => {
   return (
@@ -22,12 +22,12 @@ const Sidebar = () => {
         <div className="sidebarMenu">
           <h3 className="sidebarTitle">Dashboard</h3>
           <ul className="sidebarList">
-            <Link to="/" className="link">
-              <li className="sidebarListItem active">
+            <NavLink to="/" className="link">
+              <li className="sidebarListItem ">
                 <LineStyle className="sidebarIcon" />
                 Home
               </li>
-            </Link>
+            </NavLink>
             <li className="sidebarListItem">
               <Timeline className="sidebarIcon" />
               Analytics
@@ -41,18 +41,18 @@ const Sidebar = () => {
         <div className="sidebarMenu">
           <h3 className="sidebarTitle">Quick Menu</h3>
           <ul className="sidebarList">
-            <Link to="/users" className="link">
+            <NavLink to="/users" className="link">
               <li className="sidebarListItem">
                 <PermIdentity className="sidebarIcon" />
                 Users
               </li>
-            </Link>
-            <Link to="/products" className="link">
+            </NavLink>
+            <NavLink to="/products" className="link">
               <li className="sidebarListItem">
                 <Storefront className="sidebarIcon" />
                 Products
               </li>
-            </Link>
+            </NavLink>
             <li className="sidebarListItem">
               <AttachMoney className="sidebarIcon" />
               Transactions
