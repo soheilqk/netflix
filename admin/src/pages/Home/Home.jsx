1a4a65481c3bc0212227000d8a128f7e12a56de8
Soheil Qorbani <soheil.q.k@gmail.com>
2022-05-24 14:14:43 +0430

    chart

--- admin/src/pages/Home/Home.jsx
+++ admin/src/pages/Home/Home.jsx
@@ -1,10 +1,18 @@
+import Chart from "../../components/Chart/Chart";
 import FeaturedInfo from "../../components/FeaturedInfo/FeaturedInfo";
+import { userData } from "../../dummyData";
 import "./home.css";
 
 const Home = () => {
   return (
     <div className="home">
       <FeaturedInfo />
+      <Chart
+        data={userData}
+        title="User Analytics"
+        grid
+        dataKey="Active User"
+      />
     </div>
   );
 };
